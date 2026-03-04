885e8814ca830c69f0c52bcc06d832469a3d82b3
Felipe Feyh <felipefeyh2@gmail.com>
2026-03-03 21:03:29 -0300

    feat: projeto inicial Spring Boot com dependências base (Web, JPA, H2, Lombok)

---
+++ src/main/java/com/felipefeyh/empreendimentos/EmpreendimentosApiApplication.java
@@ -0,0 +1,13 @@
+package com.felipefeyh.empreendimentos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpreendimentosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpreendimentosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/felipefeyh/empreendimentos/EmpreendimentosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipefeyh.empreendimentos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpreendimentosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
